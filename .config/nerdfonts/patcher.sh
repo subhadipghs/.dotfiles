ecdafcd0faf9b5342246844b7f89feec6681a524
Subhadip Ghosh <ghoshsubhadipghosh@gmail.com>
2023-04-13 14:31:43 +0530

    feat: added flexible docker nerdfont patcher options

--- .config/nerdfonts/patcher.sh
+++ .config/nerdfonts/patcher.sh
@@ -1,12 +1,23 @@
 #!/bin/bash
 
-indir=$1
-outdir=$2
+version=$1
+indir=$2
+outdir=$3
+echo $indir, $outdir
+# --quiet --no-progressbars \
 
-docker run --rm \
+if [[ $version == '-v1' ]]; then
+  echo "Running ghcr.io/cdalvaro/docker-nerd-fonts-patcher:latest docker image"
+  docker run --rm \
     --volume $indir:/nerd-fonts/in \
     --volume $outdir:/nerd-fonts/out \
     --user $(id -u):$(id -g) \
     ghcr.io/cdalvaro/docker-nerd-fonts-patcher:latest \
-    --quiet --no-progressbars \
     --mono --adjust-line-height --complete --careful
+else
+  echo "Running nerdfonts/patcher docker image"
+  docker run --rm \
+    -v $indir:/in \
+    -v $outdir:/out \
+    nerdfonts/patcher --mono --adjust-line-height --complete --careful
+fi
